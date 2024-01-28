166f3e16ff1c859242fa0938d9fbfb8d08708a8b
Sam Zhang <sam.zijing.zhang@gmail.com>
2024-01-28 10:36:55 -0500

    fix help str

--- a1/interpreter.c
+++ a1/interpreter.c
@@ -121,7 +121,7 @@ help			Displays all the commands\n \
 quit			Exits / terminates the shell with “Bye!”\n \
 set VAR STRING		Assigns a value to shell memory\n \
 print VAR		Displays the STRING assigned to VAR\n \
-run SCRIPT.TXT		Executes the file SCRIPT.TXT";
+run SCRIPT.TXT		Executes the file SCRIPT.TXT\n";
 
 int my_mkdir(char *dirname);
 int my_touch(char *file);    
