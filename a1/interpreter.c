ea527c9f10ad96c39be9e4c896b6b1c3689b078a
Sam Zhang <sam.zijing.zhang@gmail.com>
2024-02-06 22:33:49 -0500

    format

--- a1/interpreter.c
+++ a1/interpreter.c
@@ -3,26 +3,24 @@
  * Sam Zhang 261072449
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/stat.h>
 #include "shell.h"
 #include "shellmemory.h"
 #include "stringUtils.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 int badcommand() {
   printf("%s\n", "Unknown Command");
   return 1;
 }
 
-
 int emptyIf() {
   printf("%s\n", "Empty if clause");
   return 1;
 }
-
 
 // For run command only
 int badcommandFileDoesNotExist() {
@@ -31,7 +29,8 @@ int badcommandFileDoesNotExist() {
 }
 
 int badCommandFrom(char *command) {
-  printf("%s%s\n", "Bad command: ", command);;
+  printf("%s%s\n", "Bad command: ", command);
+  ;
   return 1;
 }
 
@@ -40,11 +39,11 @@ int quit();
 int print(char *var);
 int set(char *var, char *value);
 int echo(char *arg);
-int my_mkdir(char *dirname);
-int my_touch(char *file);    
-int my_cd(char *dirname);   
-int my_cat(char *file);    
-int my_ls();
+int myMkdir(char *dirname);
+int myTouch(char *file);
+int myCd(char *dirname);
+int myCat(char *file);
+int myLs();
 int IfCommand(char *command_args[], int args_size);
 void resolveIdentifier(char *identifier, char *value);
 int executeCommands(char *command_args[], int start, int end);
@@ -62,7 +61,7 @@ int interpreter(char *command_args[], int args_size) {
     command_args[i][strcspn(command_args[i], "\r\n")] = 0;
   }
 
-  if (args_size < 1){
+  if (args_size < 1) {
     return badcommand();
   }
 
@@ -114,43 +113,47 @@ int interpreter(char *command_args[], int args_size) {
     if (args_size != 2)
       return badcommand();
 
-    return my_mkdir(command_args[1]);
+    return myMkdir(command_args[1]);
 
   } else if (strcmp(command_args[0], "my_touch") == 0) {
     if (args_size != 2)
       return badcommand();
 
-    return my_touch(command_args[1]);
+    return myTouch(command_args[1]);
 
   } else if (strcmp(command_args[0], "my_cd") == 0) {
     if (args_size != 2)
       return badcommand();
 
-    return my_cd(command_args[1]);
+    return myCd(command_args[1]);
 
   } else if (strcmp(command_args[0], "my_cat") == 0) {
     if (args_size != 2)
       return badcommand();
-    
-    return my_cat(command_args[1]);
+
+    return myCat(command_args[1]);
+
   } else if (strcmp(command_args[0], "my_ls") == 0) {
     if (args_size != 1)
       return badcommand();
-    
-    return my_ls();
-  } else if (strcmp(command_args[0], "if") == 0){
-    if (args_size < 7) { 
-        return emptyIf();
+
+    return myLs();
+
+  } else if (strcmp(command_args[0], "if") == 0) {
+    if (args_size < 7) { // Minimum size for a valid if-else-fi command
+      return emptyIf();
     }
 
     return IfCommand(command_args, args_size);
-  } else if (strcmp(command_args[0], "tailf") == 0){
-    if (args_size != 2) { // Minimum size for a valid if-else-fi command
-        return badcommand();
+
+  } else if (strcmp(command_args[0], "tailf") == 0) {
+    if (args_size != 2) {
+      return badcommand();
     }
+
     return tailf(command_args[1]);
-  }
-  else
+
+  } else
     return badcommand();
 }
 
@@ -200,7 +203,7 @@ int echo(char *arg) {
 
     strcpy(out_str, mem_get_value(variable_name));
 
-    strcmp("Variable does not exist", out_str) == 0 ? strcpy(out_str, "") 
+    strcmp("Variable does not exist", out_str) == 0 ? strcpy(out_str, "")
                                                     : NULL;
   } else {
     strcpy(out_str, arg);
@@ -211,46 +214,43 @@ int echo(char *arg) {
   return 0;
 }
 
+int myMkdir(char *dirname) { return mkdir(dirname, 0755); }
 
-int my_mkdir(char *dirname) {
-  return mkdir(dirname, 0755);
-}
-
-int my_cd(char *dirname) {
+int myCd(char *dirname) {
   int err_code = chdir(dirname);
 
   return err_code != 0 ? badCommandFrom("my_cd") : 0;
 }
 
-int my_touch (char* dirname) {
+int myTouch(char *dirname) {
   char command[110];
 
-  strcpy(command,"touch ");
+  strcpy(command, "touch ");
   strcat(command, dirname);
   // technically unsafe to do system calls directly, to be tested
-   fflush(stdout);
-   return system(command);
+  fflush(stdout);
+  return system(command);
 }
 
-int my_cat (char* filename) {
-    FILE *fobj;
-    char buffer[9999]; // hope that no line is over 9999 characters :) 
+int myCat(char *filename) {
+  FILE *fobj;
+  char buffer[9999]; // hope that no line is over 9999 characters :)
 
-    fobj = fopen(filename, "r");
+  fobj = fopen(filename, "r");
 
-    if (fobj == NULL) {
-      return badCommandFrom("my_cat");
-    }
+  if (fobj == NULL) {
+    return badCommandFrom("my_cat");
+  }
 
-    while (fgets(buffer, sizeof(buffer), fobj) != NULL) {
-        printf("%s", buffer);
-    }
-    
-    fclose(fobj);
-    return 0;
+  while (fgets(buffer, sizeof(buffer), fobj) != NULL) {
+    printf("%s", buffer);
+  }
+
+  fclose(fobj);
+  return 0;
 }
 
-int my_ls(){
+int myLs() {
   fflush(stdout);
   return system("ls .");
 }
@@ -282,128 +282,135 @@ int run(char *script) {
 
 int IfCommand(char *command_args[], int args_size) {
 
-    char *identifier1 = command_args[1];
-    char *op = command_args[2];
-    char *identifier2 = command_args[3];
+  char *identifier1 = command_args[1];
+  char *op = command_args[2];
+  char *identifier2 = command_args[3];
 
-    int condition = evaluateCondition(identifier1, op, identifier2);
-    if (condition == -1) {
-        return badcommand(); 
-    }
+  int condition = evaluateCondition(identifier1, op, identifier2);
+  if (condition == -1) {
+    return badcommand();
+  }
 
-    int elseIndex = 4;
-    while (elseIndex < args_size && strcmp(command_args[elseIndex], "else") != 0) {
-        elseIndex++;
-    }
+  int elseIndex = 4;
+  while (elseIndex < args_size &&
+         strcmp(command_args[elseIndex], "else") != 0) {
+    elseIndex++;
+  }
 
-    if (elseIndex == args_size || strcmp(command_args[args_size - 1], "fi") != 0) {
-        return badcommand();
-    }
+  if (elseIndex == args_size ||
+      strcmp(command_args[args_size - 1], "fi") != 0) {
+    return badcommand();
+  }
 
-    return condition ? executeCommands(command_args, 5, elseIndex) 
-                     : executeCommands(command_args, elseIndex + 1, args_size - 1);
+  return condition
+             ? executeCommands(command_args, 5, elseIndex)
+             : executeCommands(command_args, elseIndex + 1, args_size - 1);
 }
 
 void resolveIdentifier(char *identifier, char *value) {
-    if (identifier[0] == '$') {
-        char *varValue = mem_get_value(identifier + 1);
-        if (varValue != NULL) {
-            strncpy(value, varValue, 100);
-        } else {
-            strncpy(value, "", 100);
-        }
+  if (identifier[0] == '$') {
+    char *varValue = mem_get_value(identifier + 1);
+    if (varValue != NULL) {
+      strncpy(value, varValue, 100);
     } else {
-        strncpy(value, identifier, 100);
+      strncpy(value, "", 100);
     }
-    value[100] = '\0';
+  } else {
+    strncpy(value, identifier, 100);
+  }
+  value[100] = '\0';
 }
 
 int evaluateCondition(char *identifier1, char *op, char *identifier2) {
-    char value1[101], value2[101];
+  char value1[101], value2[101];
 
-    resolveIdentifier(identifier1, value1);
-    resolveIdentifier(identifier2, value2);
+  resolveIdentifier(identifier1, value1);
+  resolveIdentifier(identifier2, value2);
 
-    if (strcmp(op, "==") == 0) {
-        return strcmp(value1, value2) == 0;
-    } else if (strcmp(op, "!=") == 0) {
-        return strcmp(value1, value2) != 0;
-    }
+  if (strcmp(op, "==") == 0) {
+    return strcmp(value1, value2) == 0;
+  } else if (strcmp(op, "!=") == 0) {
+    return strcmp(value1, value2) != 0;
+  }
 
-    return -1; 
+  return -1;
 }
 
 int executeCommands(char *command_args[], int start, int end) {
-    char *args[100]; 
-    int args_count = 0; 
-    int command_start = start; 
+  char *args[100];
+  int args_count = 0;
+  int command_start = start;
 
-    for (int i = start; i < end; ++i) {
-        if (i + 1 == end || strcmp(command_args[i + 1], "else") == 0 || strcmp(command_args[i + 1], "fi") == 0) {
-            for (int j = command_start; j <= i; ++j) {
-                args[args_count++] = command_args[j];
-            }
+  for (int i = start; i < end; ++i) {
+    if (i + 1 == end || strcmp(command_args[i + 1], "else") == 0 ||
+        strcmp(command_args[i + 1], "fi") == 0) {
+      for (int j = command_start; j <= i; ++j) {
+        args[args_count++] = command_args[j];
+      }
 
-            int err_code = interpreter(args, args_count); // recursively call interpreter to evaluate the command
-            if (err_code != 0) {
-                return err_code; 
-            }
+      int err_code = interpreter(
+          args,
+          args_count); // recursively call interpreter to evaluate the command
+      if (err_code != 0) {
+        return err_code;
+      }
 
-            args_count = 0;
-            command_start = i + 2; 
-        }
+      args_count = 0;
+      command_start = i + 2;
     }
+  }
 
-    return 0; 
+  return 0;
 }
 
 int tailf(const char *filename) {
 
-    /*
-     * This command was mostly implemented by Bohan, but is my favourite command...
-     * when I follow 6 panes of real time log files at the same time, it makes me feel like a hackerman. -Sam
-     *
-     * To test this, you can run this on a file in this shell, and make changes to that file
-     * in your own shell in real time via a script or manually. New content should be displayed as they come 
-     * in.
-     */
-  
-    FILE *file = fopen(filename, "r");
-    if (!file) {
-        perror("Error opening file");
-        return 0;
-    }
+  /*
+   * This command was mostly implemented by Bohan, but is my favourite
+   * command... when I follow 6 panes of real time log files at the same time,
+   * it makes me feel like a hackerman. -Sam
+   *
+   * To test this, you can run this on a file in this shell, and make changes to
+   * that file in your own shell in real time via a script or manually. New
+   * content should be displayed as they come in.
+   */
 
-    fseek(file, 0, SEEK_END);
-    long fileSize = ftell(file);
-
-    // print tail (10 lines)
-    int lineCount = 0;
-    long position = fileSize;
-    while (position >= 0 && lineCount <= 10) {
-        fseek(file, position, SEEK_SET);
-        if (fgetc(file) == '\n') {
-            lineCount++;
-        }
-        position--;
-    }
-
-    char buffer[1000];
-    while (fgets(buffer, 1000, file)) {
-        printf("%s", buffer);
-    }
-
-    // watch for new lines and print them
-    while (1) {
-        if (fgets(buffer, 1000, file) != NULL) {
-            printf("%s", buffer);
-        } else {
-
-            usleep(500000); 
-            clearerr(file); 
-        }
-    }
-
-    fclose(file);
+  FILE *file = fopen(filename, "r");
+  if (!file) {
+    perror("Error opening file");
     return 0;
+  }
+
+  fseek(file, 0, SEEK_END);
+  long fileSize = ftell(file);
+
+  // print tail (10 lines)
+  int lineCount = 0;
+  long position = fileSize;
+  while (position >= 0 && lineCount <= 10) {
+    fseek(file, position, SEEK_SET);
+    if (fgetc(file) == '\n') {
+      lineCount++;
+    }
+    position--;
+  }
+
+  char buffer[1000];
+  while (fgets(buffer, 1000, file)) {
+    printf("%s", buffer);
+  }
+
+  // watch for new lines and print them
+  while (1) {
+    if (fgets(buffer, 1000, file) != NULL) {
+      printf("%s", buffer);
+    } else {
+
+      usleep(500000);
+      clearerr(file);
+    }
+  }
+
+  fclose(file);
+  return 0;
 }
