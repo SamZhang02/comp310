86c87781f0bdf26799a1e5af9ef8b3ebe0b13367
Sam Zhang <sam.zijing.zhang@gmail.com>
2024-03-28 14:46:55 -0400

    ??

--- a3/fs/fsutil2.c
+++ a3/fs/fsutil2.c
@@ -307,9 +307,6 @@ int defragment() {
     struct file_data *file_data = curr->data;
     fsutil_create(file_data->name, file_data->size);
     fsutil_write(file_data->name, file_data->content, file_data->size);
-
-    free(file_data->name);
-    free(file_data->content);
   }
 
   list_clear(existing_files);
